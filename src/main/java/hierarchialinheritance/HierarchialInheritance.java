bc87cf61afa53fb11e8f93fa68e7701f99444f9f
HP <HP@DESKTOP-ENDEV7H>
2023-03-09 20:13:11 +0530

    commit2

--- src/main/java/hierarchialinheritance/HierarchialInheritance.java
+++ src/main/java/hierarchialinheritance/HierarchialInheritance.java
@@ -13,8 +13,21 @@ public class HierarchialInheritance {
 		System.out.println("Salary is " + p.getSalary());
 		p.calculateSalary();
 		System.out.println("Salary is " + p.getSalary());
+
+		ContractEmployee c = new ContractEmployee();
+		c.setEmployeeId(23);
+		c.setEmployeeName("Aibin");
+		c.setPayrollCompany("Tech");
+		c.setSalary(120000);
+
+		System.out.println();
 		
-		
+		System.out.println("Name  is " + c.getEmployeeName());
+		System.out.println("Employee Id" + c.getEmployeeId());
+		System.out.println("PayrollCompany is" + c.getPayrollCompany());
+		System.out.println("Salary is " + c.getSalary());
+		c.calaculateSalary();
+		System.out.println("Salary is " + c.getSalary());
 
 	}
 
